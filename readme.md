62cbb5efe6d2bdef914d82d307953d09fecf46d8
Yash <yashkumar982000@gmail.com>
2022-10-02 16:34:38 +0530

    colors

---
+++ readme.md
@@ -0,0 +1,32 @@
+# Colors
+
+This is a multipurpose colors website developed using html, tailwindcss and javascript.
+
+![html](https://user-images.githubusercontent.com/109405477/193323207-87346e49-791a-4c7b-a9cd-26aec55d5f1f.svg) ![TailwindCSS](https://img.shields.io/badge/-%20TAILWINDCSS-blue) ![js](https://user-images.githubusercontent.com/109405477/193323275-7ff8faa7-26e3-4942-8b57-b6e9ed660cd3.svg)
+
+**Features :**
+
+1. Hex to Rgb
+2. Rgb to Hex
+3. Pick color by color range
+4. Show color by hex code
+5. Show color by rgb code
+6. Random Color Picker
+
+## Video
+
+**Check it out at : https://colorsgeneratornow.netlify.app/**
+
+## 🚀 About Me
+
+I'm Yash Kumar and i am aspiring to be a Full Stack Web Developer.
+
+Connect with me on :
+
+[![Instagram](https://img.shields.io/badge/Instagram-%23E4405F.svg?style=for-the-badge&logo=Instagram&logoColor=white)](https://www.instagram.com/theyash_yk09/)
+
+[![Twitter](https://img.shields.io/badge/Twitter-%231DA1F2.svg?style=for-the-badge&logo=Twitter&logoColor=white)](https://www.twitter.com/theyash_yk09/)
+
+## Feedback
+
+If you have any feedback, please reach out to me at theyashkumaryk09@gmail.com
---
+++ script.js
@@ -0,0 +1,164 @@
+// Hex to Rgb
+
+function hexConvbtn() {
+  let hexColor = document.getElementById("hexCInp").value;
+
+  let convertedColor = `rgb(${("0x" + hexColor[1] + hexColor[2]) | 0}, ${
+    ("0x" + hexColor[3] + hexColor[4]) | 0
+  }, ${("0x" + hexColor[5] + hexColor[6]) | 0})`;
+
+  document.getElementById("hexCOut").textContent = convertedColor;
+}
+
+// Rgb to Hex
+
+function rgbConvbtn() {
+  let rC = document.getElementById("RCInput").value;
+  let gC = document.getElementById("GCInput").value;
+  let bC = document.getElementById("BCInput").value;
+
+  function decimalToHex(val) {
+    let hexV = Number(val).toString(16);
+    if (hexV.length < 2) {
+      hexV = "0" + hexV;
+    }
+    return hexV;
+  }
+
+  function rgbToHex(r, g, b) {
+    let redV = decimalToHex(r);
+    let greenV = decimalToHex(g);
+    let blueV = decimalToHex(b);
+    return "#" + redV + greenV + blueV;
+  }
+
+  document.getElementById("rgbCOut").textContent = rgbToHex(rC, gC, bC);
+}
+
+// Range Selector
+
+function RangeSelector() {
+  let rV = document.getElementById("RrInput").value;
+  let gV = document.getElementById("GrInput").value;
+  let bV = document.getElementById("BrInput").value;
+
+  let rangeBox = document.getElementById("range-color-box");
+
+  document.getElementById("rangeCopy").textContent = `rgb(${rV},${gV},${bV})`;
+  rangeBox.style.backgroundColor = `rgb(${rV},${gV},${bV})`;
+}
+
+// Rgb Color Finder
+
+function RGBFindbtn() {
+  let red = document.getElementById("RInput").value;
+  let green = document.getElementById("GInput").value;
+  let blue = document.getElementById("BInput").value;
+
+  document.getElementById(
+    "rgb-color-box"
+  ).style.backgroundColor = `rgb(${red},${green},${blue})`;
+}
+
+// Hex Color Finder
+
+function hexFindbtn() {
+  let hexInput = document.getElementById("hexInput").value;
+  document.getElementById("color-box").style.backgroundColor = hexInput;
+}
+
+// Random Color Picker
+
+function randompickerbtn() {
+  let newColor = "#" + Math.random().toString(16).substring(2, 8);
+  let bg = document.getElementById("randomValuePara");
+  bg.textContent = newColor;
+  document.getElementById("randomPicker").style.backgroundColor = newColor;
+}
+
+// Copy Color Code
+
+function copyIt() {
+  let colorValue = document.getElementById("randomValuePara").textContent;
+  navigator.clipboard.writeText(colorValue);
+
+  let copied = document.getElementById("copied");
+  copied.style.display = "block";
+
+  setTimeout(() => {
+    copied.style.display = "block";
+  }, 100);
+
+  setTimeout(() => {
+    copied.style.display = "none";
+  }, 800);
+}
+
+function copyItOne() {
+  let colorValue = document.getElementById("hexCOut").textContent;
+  navigator.clipboard.writeText(colorValue);
+
+  let copied = document.getElementById("copied-1");
+  copied.style.display = "block";
+
+  if (!document.getElementById("hexCOut").textContent) {
+    document.getElementById("copied-1").textContent = "";
+  } else {
+    document.getElementById("copied-1").textContent = "Copied ✔";
+  }
+
+  setTimeout(() => {
+    copied.style.display = "block";
+  }, 100);
+
+  setTimeout(() => {
+    copied.style.display = "none";
+    document.getElementById("hexCInp").value = "";
+    document.getElementById("hexCOut").textContent = "";
+  }, 800);
+}
+
+function copyItTwo() {
+  let colorValue = document.getElementById("rgbCOut").textContent;
+  navigator.clipboard.writeText(colorValue);
+
+  let copied = document.getElementById("copied-2");
+  copied.style.display = "block";
+
+  if (!document.getElementById("rgbCOut").textContent) {
+    document.getElementById("copied-2").textContent = "";
+  } else {
+    document.getElementById("copied-2").textContent = "Copied ✔";
+  }
+
+  setTimeout(() => {
+    copied.style.display = "block";
+  }, 100);
+
+  setTimeout(() => {
+    copied.style.display = "none";
+    document.getElementById("RCInput").value = "";
+    document.getElementById("GCInput").value = "";
+    document.getElementById("BCInput").value = "";
+    document.getElementById("rgbCOut").textContent = "";
+  }, 800);
+}
+
+function copyItLast() {
+  //   document.getElementById("rangeCopy").style.display = "block";
+  let colorValue = document.getElementById("rangeCopy").textContent;
+  navigator.clipboard.writeText(colorValue);
+
+  let copied = document.getElementById("copied-3");
+  copied.style.display = "block";
+
+  setTimeout(() => {
+    copied.style.display = "block";
+  }, 100);
+
+  setTimeout(() => {
+    copied.style.display = "none";
+    document.getElementById("hexCInp").value = "";
+    document.getElementById("hexCOut").textContent = "";
+  }, 800);
+}
